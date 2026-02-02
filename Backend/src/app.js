9e98b92807fc8b195b6e5baa33f2564632db2647
Soham Bangar <sohambangar188@gmail.com>
2026-02-02 10:17:59 +0530

    frontend connected

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,7 +1,9 @@
 const express = require('express')
+const cors = require('cors')
 const noteModel = require('./Models/note.model')
 
 const app = express()
+app.use(cors())
 app.use(express.json())
 
 /**
